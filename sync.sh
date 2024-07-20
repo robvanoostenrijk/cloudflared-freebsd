75540ad0c9ca933f1c755c735e1ab1df12092715
Rob van Oostenrijk <robvanoostenrijk@noreply.users.github.com>
2024-07-20 15:37:43 +0400

    FreeBSD builds

---
+++ sync.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+git pull cloudflared master --rebase
+git pull cloudflared master --rebase --tags
+tag=$(curl -s "https://api.github.com/repos/cloudflare/cloudflared/tags" | jq -r '.[0].name')
+
+git tag -d "${tag}"
+git tag "${tag}" -m "cloudflared ${tag}"
